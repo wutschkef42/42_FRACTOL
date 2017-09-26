bdc9dbc1fa11ef74e3f215a3e55f9970e58a851b
Felix Wutschke <fwutschk@student.42.fr>
2017-09-26 03:01:10 +0200

    typos

--- src/cos_iterator.c
+++ src/cos_iterator.c
@@ -1,7 +1,7 @@
 
 #include "../lib/libft/libft.h"
 
-// calculates the term z(k+1) = c * sin(z(k)) note this is a complex sine
+// calculates the term z(k+1) = c * cos(z(k)) note this is a complex sine
 t_complex	cos_iterator(t_complex zk, t_complex c)
 {
 	return (cmul(c, ccosinus(zk)));
